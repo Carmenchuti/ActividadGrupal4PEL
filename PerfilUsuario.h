6df1ab1a46c49da738a2530600ee90d4495ca13c
Carmenchuti <carmenbolanosv13@gmail.com>
2025-12-05 13:31:45 +0100

    Update PerfilUsuario.h

--- PerfilUsuario.h
+++ PerfilUsuario.h
@@ -4,9 +4,20 @@
 
 #ifndef ACTIVIDADGRUPAL4PEL_PERFILUSUARIO_H
 #define ACTIVIDADGRUPAL4PEL_PERFILUSUARIO_H
-
+#include <string>
+#include "ListaEnlazada.h"
+#include "Contacto.h"
 
 class PerfilUsuario {
+private:
+    std::string alias;
+    int edad;
+    std::string descripcion;
+    ListaEnlazada<Contacto> misContactos; //Uso de la plantilla obligatoria
+public:
+    PerfilUsuario(std::string a, int e, std::string d) : alias(a),edad(e),descripcion(d) {}
+
+
 };
 
 
